3e80a9eb6c5d8fecda4d6fa3caaff40e59941f72
Bayakan <spring.boot@hotmail.com>
2024-04-04 13:27:05 +0300

    RestTemplate как взаимодействуют приложения?

---
+++ src/main/java/com/example/application1/response/Response.java
@@ -0,0 +1,11 @@
+package com.example.application1.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Response {
+    private String response;
+
+}
